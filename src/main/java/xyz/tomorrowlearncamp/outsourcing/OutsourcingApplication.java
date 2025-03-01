a831bc2fe98ae9f86273bb895ecfc83ff8506d8f
mixedsider <dmstjd0812@gmail.com>
2025-03-01 17:02:24 +0900

    docs: 프로젝트 구조 생성
    Signed-off-by: mixedsider <dmstjd0812@gmail.com>

--- src/main/java/xyz/tomorrowlearncamp/outsourcing/OutsourcingApplication.java
+++ src/main/java/xyz/tomorrowlearncamp/outsourcing/OutsourcingApplication.java
@@ -2,7 +2,9 @@ package xyz.tomorrowlearncamp.outsourcing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class OutsourcingApplication {
 
