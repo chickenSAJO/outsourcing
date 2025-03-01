a6b19bc0dd5f011f6596806576375aa9957a490d
mixedsider <dmstjd0812@gmail.com>
2025-03-01 17:23:47 +0900

    feat: swagger 적용
    Signed-off-by: mixedsider <dmstjd0812@gmail.com>

---
+++ src/main/java/xyz/tomorrowlearncamp/outsourcing/domain/comment/controller/StoreWriteCommentController.java
@@ -0,0 +1,17 @@
+package xyz.tomorrowlearncamp.outsourcing.domain.comment.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/comments")
+public class StoreWriteCommentController {
+
+    @GetMapping("")
+    public ResponseEntity<Void> swagger_test() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
