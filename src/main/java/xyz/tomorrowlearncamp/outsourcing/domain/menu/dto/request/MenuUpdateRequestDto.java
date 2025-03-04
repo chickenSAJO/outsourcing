aef47d61291389a8fadf247518781a6562cc5a0a
myoungminjun <myoungminjun@gmail.com>
2025-03-04 13:22:46 +0900

    Feat(menu): 메뉴 삭제기능 구현

--- src/main/java/xyz/tomorrowlearncamp/outsourcing/domain/menu/dto/request/MenuUpdateRequestDto.java
+++ src/main/java/xyz/tomorrowlearncamp/outsourcing/domain/menu/dto/request/MenuUpdateRequestDto.java
@@ -1,4 +1,12 @@
 package xyz.tomorrowlearncamp.outsourcing.domain.menu.dto.request;
 
+import lombok.Getter;
+
+@Getter
 public class MenuUpdateRequestDto {
+    private String menuName;
+    private String menuContent;
+    private int price;
+    private String menuImageUrl;
+    private String menuStatus;
 }
--- src/main/java/xyz/tomorrowlearncamp/outsourcing/domain/menu/dto/response/MenuAddResponseDto.java
+++ src/main/java/xyz/tomorrowlearncamp/outsourcing/domain/menu/dto/response/MenuAddResponseDto.java
@@ -7,14 +7,15 @@ public class MenuAddResponseDto {
     private final Long menuId;
     private final String menuName;
     private final String menuContent;
-    private final String imageUrl;
     private final int price;
+    private final String imageUrl;
 
-    public MenuAddResponseDto(Long menuId, String menuName, String menuContent, String imageUrl, int price) {
+
+    public MenuAddResponseDto(Long menuId, String menuName, String menuContent, int price, String imageUrl) {
         this.menuId = menuId;
         this.menuName = menuName;
         this.menuContent = menuContent;
-        this.imageUrl = imageUrl;
         this.price = price;
+        this.imageUrl = imageUrl;
     }
 }
