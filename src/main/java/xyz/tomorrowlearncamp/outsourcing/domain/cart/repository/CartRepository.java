040901a041b0be587a3450d336f2f1d8efca3f43
3uomlkh <3uomlkh@gmail.com>
2025-03-04 20:44:57 +0900

    feat(Cart): 하루 지난 장바구니 삭제 기능 구현 #5
    ### 마지막 업데이트일로부터 하루 지난 장바구니 삭제 기능
    - 매일 새벽 3시 스케줄러 실행
    - 가장 최근 추가된 메뉴의 날짜가 하루가 지났을 시 장바구니 전체 비우기
    
    Signed-off-by: 3uomlkh <3uomlkh@gmail.com>

--- src/main/java/xyz/tomorrowlearncamp/outsourcing/domain/cart/repository/CartRepository.java
+++ src/main/java/xyz/tomorrowlearncamp/outsourcing/domain/cart/repository/CartRepository.java
@@ -1,8 +1,10 @@
 package xyz.tomorrowlearncamp.outsourcing.domain.cart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import xyz.tomorrowlearncamp.outsourcing.domain.cart.entity.CartEntity;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +13,7 @@ public interface CartRepository extends JpaRepository<CartEntity, Long> {
     Optional<CartEntity> findByUserIdAndMenuId(Long userId, Long menuId);
     List<CartEntity> findByUserId(Long userId);
     void deleteByUserId(Long userId);
+
+    @Query("SELECT MAX(c.updatedAt) FROM CartEntity c")
+    LocalDateTime findLatestCartUpdatedAt();
 }
