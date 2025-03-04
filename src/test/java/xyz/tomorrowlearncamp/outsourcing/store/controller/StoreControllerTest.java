933641f5cadb8bbb5d9ba1c21e02243274575512
Iamhamster <gksdbsgml313@naver.com>
2025-03-04 17:15:53 +0900

    feat(Store): storeController의 crud관련 5가지 테스트 추가

--- src/test/java/xyz/tomorrowlearncamp/outsourcing/store/controller/StoreControllerTest.java
+++ src/test/java/xyz/tomorrowlearncamp/outsourcing/store/controller/StoreControllerTest.java
@@ -2,7 +2,6 @@ package xyz.tomorrowlearncamp.outsourcing.store.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
