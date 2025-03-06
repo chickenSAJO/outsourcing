039adb9389f5c59a1a4a663aec4299f7c3f1b88b
3uomlkh <3uomlkh@gmail.com>
2025-03-06 13:08:27 +0900

    feat(Order): AOP 적용을 위한 Order 어노테이션 생성 #12
    ### Order 어노테이션 생성 및 적용
    - Order 어노테이션 생성
    - 각 OrderController에 어노테이션 적용
    
    Signed-off-by: 3uomlkh <3uomlkh@gmail.com>

--- src/main/java/xyz/tomorrowlearncamp/outsourcing/domain/order/controller/UserOrderController.java
+++ src/main/java/xyz/tomorrowlearncamp/outsourcing/domain/order/controller/UserOrderController.java
@@ -5,8 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import xyz.tomorrowlearncamp.outsourcing.domain.order.dto.request.PlaceOrderRequestDto;
+import xyz.tomorrowlearncamp.outsourcing.domain.order.dto.response.OrderStatusResponseDto;
 import xyz.tomorrowlearncamp.outsourcing.domain.order.dto.response.PlaceOrderResponseDto;
 import xyz.tomorrowlearncamp.outsourcing.domain.order.service.UserOrderService;
+import xyz.tomorrowlearncamp.outsourcing.global.config.aop.annotation.Order;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,6 +17,7 @@ public class UserOrderController {
 
     private final UserOrderService userOrderService;
 
+    @Order
     @PostMapping
     public ResponseEntity<PlaceOrderResponseDto> placeOrder(
             @SessionAttribute(name = "LOGIN_USER") Long userId,
@@ -23,12 +26,12 @@ public class UserOrderController {
         return ResponseEntity.ok(userOrderService.placeOrder(userId, dto));
     }
 
+    @Order
     @PatchMapping("/{orderId}/cancel")
-    public ResponseEntity<Void> cancelOrder(
+    public ResponseEntity<OrderStatusResponseDto> cancelOrder(
             @SessionAttribute(name = "LOGIN_USER") Long userId,
             @PathVariable Long orderId
     ) {
-        userOrderService.cancelOrder(userId, orderId);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(userOrderService.cancelOrder(userId, orderId));
     }
 }
