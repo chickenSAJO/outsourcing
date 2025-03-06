403ee36d79c387fe4a664e9edcd5c49a4407f7d5
myeonghoon <mhoo999@naver.com>
2025-03-06 18:00:39 +0900

    refactor(Auth) : 파일 수정
    ### AuthValidationMessages
    - 인터페이스로 수정

--- src/main/java/xyz/tomorrowlearncamp/outsourcing/auth/AuthValidationMessages.java
+++ src/main/java/xyz/tomorrowlearncamp/outsourcing/auth/AuthValidationMessages.java
@@ -1,11 +1,11 @@
 package xyz.tomorrowlearncamp.outsourcing.auth;
 
-public final class AuthValidationMessages {
+public interface AuthValidationMessages {
 
-    public static final String EMAIL_VALIDATION_MESSAGE = "유효한 이메일 형식이 아닙니다.";
-    public static final String PASSWORD_VALIDATION_MESSAGE = "비밀번호는 최소 8글자 이상이어야 하며, 대소문자 포함 영문 + 숫자 + 특수문자를 최소 1글자씩 포함해야 합니다.";
+    String EMAIL_VALIDATION_MESSAGE = "유효한 이메일 형식이 아닙니다.";
+    String PASSWORD_VALIDATION_MESSAGE = "비밀번호는 최소 8글자 이상이어야 하며, 대소문자 포함 영문 + 숫자 + 특수문자를 최소 1글자씩 포함해야 합니다.";
 
-    public static final String EMAIL_DUPLICATION_MESSAGE = "이미 존재하는 이메일입니다.";
-    public static final String EMAIL_OR_PASSWORD_DIFF_MESSAGE = "이메일 또는 비밀번호가 일치하지 않습니다.";
+    String EMAIL_DUPLICATION_MESSAGE = "이미 존재하는 이메일입니다.";
+    String EMAIL_OR_PASSWORD_DIFF_MESSAGE = "이메일 또는 비밀번호가 일치하지 않습니다.";
 
 }
