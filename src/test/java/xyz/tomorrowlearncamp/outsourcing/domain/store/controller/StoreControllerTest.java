e296b4c6e32d5b2ac15a3b8e3f34f4f7dc3e1540
Iamhamster <gksdbsgml313@naver.com>
2025-03-06 09:27:25 +0900

    feat(Store): controller test코드2

--- src/test/java/xyz/tomorrowlearncamp/outsourcing/domain/store/controller/StoreControllerTest.java
+++ src/test/java/xyz/tomorrowlearncamp/outsourcing/domain/store/controller/StoreControllerTest.java
@@ -5,14 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import xyz.tomorrowlearncamp.outsourcing.domain.menu.dto.response.MenuResponseDto;
+import xyz.tomorrowlearncamp.outsourcing.domain.menu.entity.MenuType;
 import xyz.tomorrowlearncamp.outsourcing.domain.store.dto.*;
-import xyz.tomorrowlearncamp.outsourcing.domain.store.entity.StoreEntity;
 import xyz.tomorrowlearncamp.outsourcing.domain.store.service.StoreService;
-import xyz.tomorrowlearncamp.outsourcing.domain.user.entity.UserEntity;
-import xyz.tomorrowlearncamp.outsourcing.domain.user.enums.Usertype;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -20,7 +19,7 @@ import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -28,7 +27,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static xyz.tomorrowlearncamp.outsourcing.domain.menu.entity.MenuType.ACTIVE;
 
 @WebMvcTest(StoreController.class)
 public class StoreControllerTest {
@@ -49,15 +47,7 @@ public class StoreControllerTest {
     @Test
     public void 가게를_생성한다() throws Exception {
         // given
-//        UserEntity mockUser = new UserEntity(
-//                "test@naver.com",
-//                "Test1234!23",
-//                "010-1234-5678",
-//                "사장닉네임",
-//                "사장명",
-//                "서울시 강남구",
-//                Usertype.OWNER
-//        );
+        Long userId = 1L;
         SaveStoreRequestDto request = new SaveStoreRequestDto(
                 "국밥맛집",
                 "09:00:00",
@@ -72,11 +62,14 @@ public class StoreControllerTest {
                 15000,
                 "사장님"
         );
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("LOGIN_USER", userId);
 
         given(storeService.saveStore(any(Long.class), any(SaveStoreRequestDto.class))).willReturn(responseDto);
 
         // when & then
         mockMvc.perform(post("/api/v1/stores")
+                        .session(session) //세션
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
@@ -89,6 +82,7 @@ public class StoreControllerTest {
     @Test
     public void 가게의_정보를_수정한다() throws Exception {
         // given
+        Long userId = 1L;
         UpdateStoreResponseDto responseDto = new UpdateStoreResponseDto(
                 1L,
                 "국밥맛집",
@@ -96,12 +90,16 @@ public class StoreControllerTest {
                 LocalTime.of(21,0,0),
                 15000
         );
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("LOGIN_USER", userId);
 
         given(storeService.updateStore(any(Long.class), any(Long.class), any(UpdateStoreRequestDto.class))).willReturn(responseDto);
 
         // when & then
         mockMvc.perform(put("/api/v1/stores/{storeId}", responseDto.getStoreId())
-                        .contentType(MediaType.APPLICATION_JSON))
+                        .session(session)
+                        .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(responseDto)))
                 .andExpect(status().isOk());
     }
 
@@ -111,6 +109,7 @@ public class StoreControllerTest {
     @Test
     public void 모든_가게를_본다() throws Exception {
         // given
+        Long userId = 1L;
         List<StoreResponseDto> responseDto = new ArrayList<>();
         responseDto.add(new StoreResponseDto(
                 1L,
@@ -119,12 +118,14 @@ public class StoreControllerTest {
                 LocalTime.of(21,0,0),
                 15000
         ));
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("LOGIN_USER", userId);
 
         given(storeService.findAllStore(any(Long.class))).willReturn(responseDto);
 
         // when & then
         mockMvc.perform(get("/api/v1/stores")
-                        .contentType(MediaType.APPLICATION_JSON))
+                        .session(session))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.size()").value(1)) //id=1만 사용
                 .andExpect(jsonPath("$[0].name").value("국밥맛집"))  //가게 이름
@@ -134,10 +135,11 @@ public class StoreControllerTest {
 
 
     //findOneStore 테스트
-    /*todo: 메뉴 부분 추가 요망*/
+    //메뉴 추가 완료
     @Test
     public void 가게와_메뉴를_확인한다() throws Exception {
         // given
+        Long userId = 1L;
         Long menuId = 1L;
         Long storeId = 1L;
         MenuResponseDto menuResponseDto = new MenuResponseDto(
@@ -146,7 +148,7 @@ public class StoreControllerTest {
                 "10",
                 10000,
                 "img.jpg",
-                ACTIVE,
+                MenuType.ACTIVE,
                 storeId
         );
         List<MenuResponseDto> menuList = new ArrayList<>();
@@ -159,17 +161,22 @@ public class StoreControllerTest {
             15000,
             menuList
         );
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("LOGIN_USER", userId);
 
         given(storeService.findOneStore(any(Long.class), any(Long.class))).willReturn(responseDto);
 
         // when & then
-        mockMvc.perform(get("/api/v1/stores/{storeId}?userId=1", responseDto.getStoreId())
-                .contentType(MediaType.APPLICATION_JSON))
-//        mockMvc.perform(get("/api/v1/stores/{storeId}",responseDto.getStoreId())
-//                        .contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/api/v1/stores/{storeId}", responseDto.getStoreId())
+                        .session(session)
+                        .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value("국밥맛집"))  //가게 이름
-                .andExpect(jsonPath("$.minimumOrder").value(15000));  //최소주문금액
+                .andExpect(jsonPath("$.name").value("국밥맛집"))
+                .andExpect(jsonPath("$.minimumOrder").value(15000))
+                .andExpect(jsonPath("$.menuList[0].name").value("햄버거"))
+                .andExpect(jsonPath("$.menuList[0].price").value(10000));
+
     }
 
 
@@ -178,13 +185,18 @@ public class StoreControllerTest {
     @Test
     public void 가게를_폐업한다() throws Exception {
         // given
+        Long userId = 1L;
         Long storeId = 1L;
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("LOGIN_USER", userId);
 
         doNothing().when(storeService).deleteStore(any(Long.class), any(Long.class));//삭제와 같이, 출력 아무것도 안할때, 사용하는 given = doNothing
 
         // when & then
         mockMvc.perform(delete("/api/v1/stores/{storeId}", storeId)
+                        .session(session)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNoContent());
+        verify(storeService, times(1)).deleteStore(any(Long.class), any(Long.class));
     }
 }
