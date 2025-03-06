1c49fb2511e6b17c221808a7a7f3bf1c3a8a5fec
mixedsider <dmstjd0812@gmail.com>
2025-03-06 15:55:24 +0900

    feat(auth): 권한값 추가
    Signed-off-by: mixedsider <dmstjd0812@gmail.com>

--- src/main/java/xyz/tomorrowlearncamp/outsourcing/domain/user/enums/ErrorUserMessage.java
+++ src/main/java/xyz/tomorrowlearncamp/outsourcing/domain/user/enums/ErrorUserMessage.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public enum ErrorUserMessage {
     NOT_FOUND_USER("없는 유저입니다."),
     INVALID_PASSWORD("비밀번호를 잘못 입력하셨습니다."),
+    UNAUTHORIZED("권한이 없습니다."),
     ;
 
     private final String errorMassage;
